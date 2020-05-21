0b03de972c80c73e8f7e773619f5c72f24739553
wangzhen <zzuwangzhen@outlook.com>
2020-05-21 13:28:43 +0800

    1.0.0

---
+++ src/SuperDB.MySQL/SuperDB.MySQL/MySQLFactory.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+
+using SuperDB.Config;
+
+using System.Data;
+
+namespace SuperDB.MySQL
+{
+    public class MySQLFactory : IDBFactory
+    {
+        private IDbConnection _Connection;
+        private IDbTransaction _Transaction;
+
+        public static MySQLFactory Create()
+        {
+            return new MySQLFactory();
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                if (_Connection == default)
+                {
+                    _Connection = new MySqlConnection(DBConfig.ConnectionString);
+                }
+                if (_Connection.State != ConnectionState.Open)
+                {
+                    _Connection.Open();
+                }
+                return _Connection;
+            }
+        }
+
+        public IDbTransaction Transaction
+        {
+            get
+            {
+                if (_Transaction == default)
+                {
+                    _Transaction = Connection.BeginTransaction();
+                }
+                return _Transaction;
+            }
+        }
+
+        public bool Commit()
+        {
+            Transaction.Commit();
+            return true;
+        }
+
+        public void Dispose()
+        {
+            _Connection?.Dispose();
+            _Connection = default;
+            _Transaction = default;
+        }
+
+        public bool Rollback()
+        {
+            Transaction.Rollback();
+            return false;
+        }
+    }
+}
